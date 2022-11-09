0d77ceee331875d69f6fb36b852c6370a22e36f7
meenakumarigandu <meenakumarigandu@gmail.com>
2022-11-09 22:24:43 +0530

    added navbar

--- src/app/components/designation/getdesignation/getdesignation.component.ts
+++ src/app/components/designation/getdesignation/getdesignation.component.ts
@@ -11,8 +11,14 @@ import { DesignationService } from 'src/app/services/designation.service';
 export class GetdesignationComponent implements OnInit {
 
   Designation:Designation[]=[];
-  sortBy: string;
-  searchText: string;
+  searchText:any;
+title='pagination';
+
+POSTS:any;
+page:number=1;
+count:number=0;
+tableSize:number=10;
+tablesize:any=[5,10,15,20]
   designation:Designation={
     designationID:0,
     designationName:'',
@@ -20,25 +26,35 @@ export class GetdesignationComponent implements OnInit {
     departmentName:''
   }
   constructor(private route:ActivatedRoute,private designationservice:DesignationService,private router:Router)
-   { this.searchText = '';
-   this.sortBy = '';}
+   {}
 
 
   ngOnInit(): void {
- 
-    this.designationservice.getAllDesignations()
-    .subscribe({
-      next:(Designation)=>
-      {
-        this.Designation=Designation;
-      },
-      error:(response)=>{
-        console.log(response);
-      }
-    })
-    
+    this.getAllDesignations() 
   }
-  
+  getAllDesignations():void
+{ this.designationservice.getAllDesignations()
+  .subscribe({
+    next:(Designation)=>
+    {
+      this.Designation=Designation;
+    },
+    error:(response)=>{
+      console.log(response);
+    }
+  })
+} 
+onTableDataChange(event:any)
+{
+  this.page=event;
+  this.getAllDesignations();
+}
+onTableSizeChange(event:any):void
+{
+  this.tableSize=event.target.value;
+  this.page=1;
+  this.getAllDesignations();
+}
 
 }
 
--- src/app/components/requestleave/getallleave/getallleaves/getallleaves.component.ts
+++ src/app/components/requestleave/getallleave/getallleaves/getallleaves.component.ts
@@ -10,6 +10,14 @@ import { RequestleaveService } from 'src/app/services/requestleave.service';
 })
 export class GetallleavesComponent implements OnInit {
 RequestLeave:RequestLeave[]=[];
+searchText:any;
+title='pagination';
+
+POSTS:any;
+page:number=1;
+count:number=0;
+tableSize:number=10;
+tablesize:any=[5,10,15,20]
 requestleave:RequestLeave={
   id:0,
   leaveType:'',
@@ -23,6 +31,9 @@ requestleave:RequestLeave={
 
 
   ngOnInit(): void {
+    this.getAllLeaves()
+  }
+  getAllLeaves():void{  
     this.requestleaveservice.getAllLeaves()
     .subscribe({
       next:(RequestLeave)=>
@@ -32,8 +43,18 @@ requestleave:RequestLeave={
       error:(response)=>{
         console.log(response);
       }
-    })
-    
-  }
-  }
+    })}
+    onTableDataChange(event:any)
+    {
+      this.page=event;
+      this.getAllLeaves();
+    }
+    onTableSizeChange(event:any):void
+    {
+      this.tableSize=event.target.value;
+      this.page=1;
+      this.getAllLeaves();
+    }
+  
+}
 
